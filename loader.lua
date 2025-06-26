46271028caa4886e3b016b5ff1c0611bc9cb0a25
SproutX-Scripter <aroaenix@gmail.com>
2025-06-27 04:59:34 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,6 @@
+-- Load your custom loading screen
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SproutX-Scripter/Dark-SpawnerV1/main/loadscreen.lua"))()
+
+-- Load your main script
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SproutX-Scripter/Dark-SpawnerV1/main/Dark-Spawner.lua"))()
 
